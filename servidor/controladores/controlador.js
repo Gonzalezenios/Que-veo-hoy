f23b1ad9d39fc953917d53997d10c4285567366c
gonzalezenios <gonzalezeniood@gmail.com>
2020-02-29 17:46:48 -0300

    modified

--- servidor/controladores/controlador.js
+++ servidor/controladores/controlador.js
@@ -1,23 +1,30 @@
 //importar la conexion a la base de datos -Enio
-var con = require('../lib/conexionbd');
+var conexion = require('../lib/conexionbd');
 
 //funcion para devolver las peliculas - Enio
 function obtenerPeliculas(req, res) {
-    var sql = 'selec * from peliculas'
-    con.query(sql, function (error, resultado, fields) {
+    //var peliculas = req.query.peliculas;
+    conexion.connect()
+
+    console.log("conectado!")
+    // se obtiene el query param peliculas
+    var sql = 'select * from peliculas;'
+    
+    conexion.query(sql, function (error, resultado, fields) {
         if (error) {
             console.log('Hubo un error en la consulta', error.message);
-            return res.status(404).send('Hubo un error en la consulta');
+            return res.status(500).send('Hubo un error en la consulta');
         }
         var response = {
             'peliculas': resultado
         };
 
         res.send(JSON.stringify(response));
-        
     });
+
+    // conexion.destroy()
 }
-    //Se expportan las funciones creadas
+    //Se expportan las funciones creadas - Enio
     module.exports = {
         obtenerPeliculas: obtenerPeliculas
     };
